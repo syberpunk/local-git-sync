848fdb94e65eef062bd4c4c07a645f1cf36ab1e6
syberpunk <eyal.eshcol@gmail.com>
2016-12-20 13:48:32 +0200

    Update simple.js

--- examples/simple.js
+++ examples/simple.js
@@ -18,3 +18,13 @@ console.log('git.log() => ' + git.log());
 // Date:   Mon Dec 19 17:02:46 2016 +0200
 //
 //  //GIT LOG TEST
+
+console.log('git.log() => ' + git.log());
+// e.g. 
+// {
+//   id: 393b24d4ae3d944eb6af2008246e4e22c37e0f2d,
+//   revision: 1359,
+//   date: Mon Dec 19 17:02:46 2016 +0200,
+//   author: syberpunk,
+//   message: //GIT LOG TEST
+// }
