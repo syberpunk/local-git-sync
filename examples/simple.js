49a2760db3bb6175a54ff8a97aee99632b33971e
syberpunk <eyal.eshcol@gmail.com>
2016-12-20 13:11:47 +0200

    Update simple.js

--- examples/simple.js
+++ examples/simple.js
@@ -10,3 +10,11 @@ console.log('git.long() => ' + git.long());
 
 console.log('git.branch() => ' + git.branch());
 // e.g. master
+
+console.log('git.log() => ' + git.log());
+// e.g. 
+// commit 393b24d4ae3d944eb6af2008246e4e22c37e0f2d
+// Author: syberpunk <syberpunk@email.com>
+// Date:   Mon Dec 19 17:02:46 2016 +0200
+//
+//  //GIT LOG TEST
